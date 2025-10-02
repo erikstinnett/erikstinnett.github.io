448a051b25bc8ef972dd5d376b3c990c73040f57
Erik Stinnett <estinnett@cub.uca.edu>
2025-10-02 14:35:04 -0500

    Website forgot to commit lololol

--- source/scripts/nav.js
+++ source/scripts/nav.js
@@ -6,3 +6,13 @@ document.addEventListener("scroll", function () {
     navbar.classList.remove("scrolled");
   }
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("open");
+  const expanded = hamburger.getAttribute("aria-expanded") === "true" || false;
+  hamburger.setAttribute("aria-expanded", !expanded);
+});
